5fffd95170c63273c4f259e25f87132b94b8f8dc
YURA <liparg104@gmail.com>
2019-12-25 12:11:40 +0200

    123

--- Лекція 5 ( array methods )/script.js
+++ Лекція 5 ( array methods )/script.js
@@ -22,19 +22,3 @@ console.log(res);
 // 2 створити масив з 10 елементів, заповнити його цілими рандомними значеннями(Math.random)
 //
 // 3 є масив
-//
-// let users = [
-//     {name: 'vasya', age: 31, status: false},
-//     {name: 'petya', age: 30, status: true},
-//     {name: 'kolya', age: 29, status: true},
-//     {name: 'olya', age: 28, status: false},
-//     {name: 'max', age: 30, status: true},
-//     {name: 'anya', age: 31, status: false},
-//     {name: 'oleg', age: 28, status: false},
-//     {name: 'andrey', age: 29, status: true},
-//     {name: 'masha', age: 30, status: true},
-//     {name: 'olya', age: 31, status: false},
-//     {name: 'max', age: 31, status: true}
-// ];
-// - відсортувати його за  віком (зростання , а потім окремо спадання)
-// - відсортувати його за кількістю знаків в імені  (зростання , а потім окремо спадання)
