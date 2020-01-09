c3224815333ea68a1b86d838e0e5eeefeb3fbeac
kvartsianyi <liparg104@gmail.com>
2020-01-09 12:50:31 +0200

    dz

--- lesson7 (OOP prototypes)/dz/script.js
+++ lesson7 (OOP prototypes)/dz/script.js
@@ -70,7 +70,7 @@ class SuperCar extends Car {
     }
 }
 
-let superCar = new SuperCar('lada', 1998, 90353, 340, 3000, false, true, 200)
+let superCar = new SuperCar('lada', 1998, 90353, 340, 3000, false, true, 200);
 superCar.info();
 
 let superCarsCollection = [];
@@ -88,9 +88,103 @@ console.log(superCarsCollection);
 // - категорию
 // - Магазин
 
+console.log('------Task3------');
+
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Category {
+    constructor(name, products) {
+        this.name = name;
+        this.products = products;
+    }
+}
+
+class Shop {
+    constructor(name, workHours, categories) {
+        this.name = name;
+        this.workHours = workHours;
+        this.categories = categories;
+    }
+
+    showProducts() {
+        for (const category of this.categories) {
+            console.log(`\nCategory ${category.name}`);
+            category.products.forEach(value => console.log(`${value.name} - ${value.price}$`));
+        }
+    }
+}
+
+let computerCategory = new Category('computers', [new Product('ASUS', 400), new Product('DELL', 550), new Product('HP', 480)]);
+let smartphoneCategory = new Category('smartphones', [new Product('Xiaomi', 250), new Product('Apple', 900)]);
+
+let smartShop = new Shop('SmartShop', '9:00 - 22:00', [computerCategory, smartphoneCategory]);
+
+console.log(`${smartShop.name}\nWorks: ${smartShop.workHours}`);
+smartShop.showProducts();
+
 // 3. взять за основу класс юзера из задания про классы добавить при помощи prototype 4 любых метода.
-//
-//     Взять библиотечный класс Array и добавить ему новый метод который
+
+console.log('------Task4------');
+
+function User(name, age, car, company) {
+    this.name = name;
+    this.age = age;
+    this.car = car;
+    this.company = company;
+}
+
+User.prototype.greeting = function () {
+    console.log(`Hello from ${this.name}`)
+};
+User.prototype.getAge = function () {
+    console.log(`My age is ${this.age}`)
+};
+User.prototype.buyCar = function (car) {
+    this.car = car;
+    console.log(`Now my car is ${this.car}`);
+};
+User.prototype.changeCompany = function (company) {
+    this.company = company;
+    console.log(`My new company is ${this.company}`)
+};
+
+
+let user = new User('Yura', 20, false, 'Oktenweb');
+user.greeting();
+user.getAge();
+user.buyCar('Audi a8');
+user.changeCompany('Oktenweb University');
+console.log(user);
+
+// Взять библиотечный класс Array и добавить ему новый метод который
 // - при вызове выводит на консоль текущий массив
 // - выводит только парные елементы
 // - выводит только элементы с парными индексами
+
+console.log('------Task4------');
+
+Array.prototype.log = function () {
+    console.log(this)
+};
+
+Array.prototype.even = function () {
+    console.log(this.filter((value) => value % 2 === 0));
+};
+
+Array.prototype.evenIndex = function () {
+    console.log(this.filter(((value, index) => index % 2 === 0)));
+};
+
+
+let arr = [1, 5, 4, 7, 2, 97];
+console.log('Масив:');
+arr.log();
+console.log('Парні елементи:');
+arr.even();
+console.log('Елементи з парними індексами:');
+arr.evenIndex();
