23d4d5b481b0579dc9a8e71306b02bb30c9ed9fe
YURA <liparg104@gmail.com>
2020-01-15 17:47:03 +0200

    add wordCounter task

--- palindrom_dz/script.js
+++ palindrom_dz/script.js
@@ -8,3 +8,22 @@ String.prototype.palindrom = function() {
 
 let str = '001'.palindrom();
 console.log(str);
+
+
+// Дано предложение. Напишите функцию, которая возвращает количество слов, которые начинаются с большой буквы.
+function wordCounter(str) {
+    let wordsCount = 0;
+    let strArr = str.split('');
+
+    for (let i = 0; i < strArr.length; i++) {
+        if(strArr[i] > 'A' && strArr[i] < 'Z')
+            if (i === 0)
+                wordsCount++;
+            else if(strArr[i-1] === ' ')
+                wordsCount++;
+    }
+
+    return wordsCount;
+}
+
+console.log(wordCounter('sDgo Dfhgid Sgjoksdf'));;
